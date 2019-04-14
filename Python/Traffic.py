247024400ca32c433e881647e9aa594a7dad4034
Alex <alexfbr3@gmail.com>
2019-04-14 09:44:36 +0300

    Ноы=вые файлы

--- Python/Traffic.py
+++ Python/Traffic.py
@@ -18,6 +18,13 @@ import matplotlib.pyplot as plt
 import csv
 print("версия csv: {}".format(csv.__version__))
 from sklearn.datasets import make_blobs
+from sklearn.metrics.cluster import silhouette_score
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
+
 
 df = pd.read_csv("D:/Traffic.csv",header=None, sep=';')
 print(df)
@@ -36,7 +43,6 @@ Y = df[1]
 print("Массив Y: {}",Y)
 print("форма массива Y: {}".format(Y.shape))
 
-from sklearn.cluster import KMeans
 # генерируем синтетические двумерные данные строим модель кластеризации
 
 
@@ -48,6 +54,7 @@ print(kmeans.predict(df))
 mglearn.discrete_scatter(df[0], df[1], kmeans.labels_, markers='o')
 mglearn.discrete_scatter(
 kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1],[0, 1, 2,3,4,5], markers='^', markeredgewidth=2)
+plt.legend(["кластер 1", "кластер 2","кластер 3","кластер 4","кластер 5", "кластер 6",], loc='best')
 plt.close()
 
 
@@ -67,14 +74,10 @@ print("y.shape:", y.shape)
 plt.plot(X, y, 'o')
 plt.close()
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
-
-#scaler = StandardScaler()
-scaler = MinMaxScaler()
+scaler = StandardScaler()
+#scaler = MinMaxScaler()
 scaler.fit(dataset)
 X_scaled = scaler.transform(dataset)
-from sklearn.cluster import DBSCAN
 
 dbscan = DBSCAN(eps=0.005, min_samples=30)
 clusters = dbscan.fit_predict(X_scaled)
@@ -91,13 +94,6 @@ plt.close()
 
 
 
-from sklearn.metrics.cluster import silhouette_score
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.cluster import KMeans
-from sklearn.cluster import DBSCAN
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
-
 
 X = dataset[:,:2]
 # масштабируем данные так, чтобы получить нулевое среднее и единичную дисперсию
@@ -112,6 +108,7 @@ random_clusters = random_state.randint(low=0, high=2, size=len(X))
 # выводим на графике результаты случайного присвоения кластеров
 axes[0].scatter(X_scaled[:, 0], X_scaled[:, 1], c=random_clusters,cmap=mglearn.cm3, s=60)
 axes[0].set_title("Случайное присвоение кластеров: {:.2f}".format(silhouette_score(X_scaled, random_clusters)))
+
 
 algorithms = [KMeans(n_clusters=6), AgglomerativeClustering(n_clusters=6),DBSCAN()]
 for ax, algorithm in zip(axes[1:], algorithms):
